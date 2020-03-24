19416679e44da3e965ec0ef6894145e2de9ac304
ZohaibZaidi <m.zohaibzaidi@gmail.com>
2020-03-24 09:56:23 +0400

    added networking module and player controller

---
+++ Assets/Scripts/Player/PlayerNetworkController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerNetworkController : NetworkBehaviour
+{
+    CharacterController controller;
+    PlayerMovement playerMovement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            controller = GetComponent<CharacterController>();
+            playerMovement = GetComponent<PlayerMovement>();
+
+            StartCoroutine(AssignPosition());
+        }
+    }
+
+    IEnumerator AssignPosition()
+    {
+        yield return new WaitForEndOfFrame();
+        controller.enabled = false;
+        playerMovement.enabled = false;
+        yield return new WaitForEndOfFrame();
+
+        NetworkManager nm = FindObjectOfType<NetworkManager>();
+        int totalPlayers = nm.numPlayers;
+
+        GameObject spawnParent = GameObject.Find("SpawnPoints");
+        transform.position = spawnParent.transform.GetChild(totalPlayers - 1).position;
+        yield return new WaitForEndOfFrame();
+        controller.enabled = true;
+        playerMovement.enabled = true;
+    }
+
+}
